2ccd00878b09eab3dd8771bf13b077df4f7bb9a4
suryapratapctrl <suryapratapsr1709@gmail.com>
2026-07-17 20:15:22 +0530

    metrics

---
+++ scikit-learn/metrics.py
@@ -0,0 +1,17 @@
+from sklearn.metrics import accuracy_score,precision_score,recall_score, f1_score
+
+
+#true answers (what actually happened)
+y_true=[1,0,1,1,0,1,0]
+
+#model's predictions (what is guessed)
+y_pred=[1,0,1,0,0,1,1]
+
+#evaluation 
+print('Accuracy: ',accuracy_score(y_true,y_pred))
+print('Precision: ',precision_score(y_true,y_pred))
+print('Recall: ',recall_score(y_true,y_pred))
+print('F1 score: ',f1_score(y_true,y_pred))
+
+
+

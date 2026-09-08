cfb021d51a6e3fdf4d40c6c74235b4461514dab6
suryapratapctrl <suryapratapsr1709@gmail.com>
2026-09-08 22:59:34 +0530

    training loop

--- learning-pytorch/1.3training-loop.py
+++ learning-pytorch/1.3training-loop.py
@@ -1,25 +1,29 @@
 import torch
 
-x=torch.tensor([1.,2.,3.,4.])
+x = torch.tensor([1., 2., 3., 4.])
 
-y_true=torch.tensor([2.,4.,6.,8.]) # correct numbers
+y_true = torch.tensor([2., 4., 6., 8.])  # correct numbers
 
-w=torch.tensor(0.0, requires_grad=True)
+w = torch.tensor(0.0, requires_grad=True)
 
-learning_rate=0.1
-epochs=10 # how many times ai needs to learn
+learning_rate = 0.1
+
+epochs = 10  # how many times AI needs to learn
 
 for epoch in range(epochs):
 
-  # step1 predicition
-  y_pred=w*x
+    # Step 1: Prediction
+    y_pred = w * x
 
-  # step2 loss
-  loss=((y_pred - y_true)**2).mean()
+    # Step 2: Loss
+    loss = ((y_pred - y_true) ** 2).mean()
 
-  loss.backward()
+    # Step 3: Calculate gradient
+    loss.backward()
 
-  with torch.no_grad():
-    w-= learning_rate*w.grad
+    # Step 4: Update weight
+    with torch.no_grad():
+        w -= learning_rate * w.grad
 
-print(f'epoch {epoch+1}: w={w.item():.4f}, loss={loss.item():.4f}')    
+    # Step 5: Print every epoch
+    print(f'Epoch {epoch + 1}: w={w.item():.4f}, loss={loss.item():.4f}')
